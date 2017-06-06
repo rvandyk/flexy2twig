6c3035d6b0f0842136c3bfaaa6074b0cbd32d53a
Rémy RVD. Van Dyk <rvandyk@axess.local>
2017-06-06 16:08:16 +0200

    gui

--- twigparse.py
+++ twigparse.py
@@ -1,35 +1,31 @@
 import re
-import sys
 from collections import deque
 
-f = open(sys.argv[1], 'r')
-o = open(sys.argv[2], 'w')
-print(sys.argv[2])
+def parse(fin,fout):
+    f = open(fin, 'r')
+    o = open(fout, 'w')
 
-currentbox = deque()
+    currentbox = deque()
 
-for line in f:
-    if(re.search(r"({if)", line)):
-        currentbox.append("if")
-    if(re.search(r"({foreach)", line)):
-        ex = re.split('foreach|\t|,|:|{|}|\n',line)
-        ex = [x for x in ex if x != '']
-        res = '{ for ' + ex[1]
-        for i in range (2,len(ex)):
-            res += ','+ ex[i]
-        res += ' in ' + ex[0] + " }"
-        line = re.sub(r"({foreach:)(.*)[}]+", res, line)
-        currentbox.append("for")
-    line = re.sub(r"[:]", " ", line)
-    line = re.sub(r"[{]", "{% ", line)
-    line = re.sub(r"[}]", " %}", line)
-    if(re.search(r"({% end)",line)):
-        line = re.sub(r"({% end)", "{% end" + currentbox.pop(), line)
-    line = re.sub(r"<flexy include src=", "{% include ", line)
-    line = re.sub(r"></flexy include>", " %}", line)
-    line = re.sub(r"\#", "\"", line)
+    for line in f:
+        if(re.search(r"({if)", line)):
+            currentbox.append("if")
+        if(re.search(r"({foreach)", line)):
+            ex = re.split('foreach|\t|,|:|{|}|\n',line)
+            ex = [x for x in ex if x != '']
+            res = '{ for ' + ex[1]
+            for i in range (2,len(ex)):
+                res += ','+ ex[i]
+            res += ' in ' + ex[0] + " }"
+            line = re.sub(r"({foreach:)(.*)[}]+", res, line)
+            currentbox.append("for")
+        line = re.sub(r"[:]", " ", line)
+        line = re.sub(r"[{]", "{% ", line)
+        line = re.sub(r"[}]", " %}", line)
+        if(re.search(r"({% end)",line)):
+            line = re.sub(r"({% end)", "{% end" + currentbox.pop(), line)
+        line = re.sub(r"<flexy include src=", "{% include ", line)
+        line = re.sub(r"></flexy include>", " %}", line)
+        line = re.sub(r"\#", "\"", line)
 
-
-
-
-    o.write(line)
+        o.write(line)
