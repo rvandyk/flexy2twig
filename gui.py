6ad09c64b94c4f759d4e518792a37767945c8b61
Rémy RVD. Van Dyk <rvandyk@axess.local>
2017-06-07 10:10:08 +0200

    diff log

--- gui.py
+++ gui.py
@@ -1,14 +1,18 @@
 from tkinter import *
 from tkinter import filedialog
-from twigparse import parse
+from twigparse import parse, difflog
 import os
 
 root = Tk()
 root.title("F2T")
 root.geometry()
 
+diff = IntVar()
+labelok = Label(root, text="DONE !",fg="green")
+
 
 def convert():
+    labelok.pack_forget()
     fin = pathin.get()
 
     head, tail = os.path.split(fin)
@@ -16,11 +20,18 @@ def convert():
     fout = pathout.get() + '/' + fname[0] + ".twig"
 
     parse(fin,fout)
+    if(diff.get() == 1):
+        difflog(fin,fout)
+    labelok.pack(side=BOTTOM)
+
 
 def loadfilein():
+    labelok.pack_forget()
     urlin.set(filedialog.askopenfilename())
 def loadfileout():
+    labelok.pack_forget()
     urlout.set(filedialog.askdirectory())
+
 
 
 labelin = Label(root, text="Flexy file path :")
@@ -31,6 +42,9 @@ labelout = Label(root, text="Twig folder path:")
 butout = Button(root, text = "Parcourir", command = loadfileout)
 urlout = StringVar(root,"/")
 pathout = Entry(root,textvariable = urlout)
+
+c = Checkbutton(root, text="Generate diff log", variable=diff)
+
 conv = Button(root, text="Convert", command = convert)
 
 
@@ -41,7 +55,8 @@ butin.pack(side=LEFT)
 labelout.pack(side=LEFT)
 pathout.pack(side=LEFT)
 butout.pack(side=LEFT)
-conv.pack(side=RIGHT)
+c.pack(side=BOTTOM)
+conv.pack(side=BOTTOM)
 
 
 
--- twigparse.py
+++ twigparse.py
@@ -1,6 +1,37 @@
 import re
 from collections import deque
 
+def difflog(fin,fout):
+    n = 0
+    with open(fin, 'r') as file1:
+        with open(fout, 'r') as file2:
+                with open('log.txt', 'w') as file_out:
+                    art = '''
+  _____ ____ _____   ____ ___ _____ _____   _     ___   ____
+ |  ___|___ \_   _| |  _ \_ _|  ___|  ___| | |   / _ \ / ___|
+ | |_    __) || |   | | | | || |_  | |_    | |  | | | | |  _
+ |  _|  / __/ | |   | |_| | ||  _| |  _|   | |__| |_| | |_| |
+ |_|   |_____||_|   |____/___|_|   |_|     |_____\___/ \____|
+
+                   '''
+                    file_out.write(art + "\n\n\n\n")
+                    while(True):
+                        n += 1
+                        l1 = file1.readline()
+                        l2 = file2.readline()
+                        if(not l1):
+                            file_out.write("\n\n\n#######\nLINE " + str(n) + "\n#######\n\n\n")
+                            file_out.write("FLEXY : NOTHING" + "\n====TWIG====> " + l2 + "\n\n--------------------------------------------------------------")
+                        if(not l2):
+                            file_out.write("\n\n\n#######\nLINE " + str(n) + "\n#######\n\n\n")
+                            file_out.write("FLEXY : " + l1 +  " \n====TWIG====> " + "NOTHING" + "\n\n--------------------------------------------------------------")
+                        if((not l1) and (not l2)):
+                            break
+                        if(l1 != l2):
+                            file_out.write("\n\n\n#######\nLINE " + str(n) + "\n#######\n\n\n")
+                            file_out.write("FLEXY : " + l1 +  " \n====TWIG====> " + l2 + "\n\n--------------------------------------------------------------")
+
+
 def parse(fin,fout):
     f = open(fin, 'r')
     o = open(fout, 'w')
@@ -18,7 +49,6 @@ def parse(fin,fout):
         if(m):
             line = ""
             m.groupdict()
-            print(m['var_name'] + '=' + m['var_value'])
             vardict.update({m['var_name'] :  m['var_value']})
 
         if(re.search(r"({if)", line)):
@@ -43,9 +73,7 @@ def parse(fin,fout):
             line = re.sub(r"></flexy include>", " %}", line)
             line = re.sub(r"\#", "\"", line)
         if(script):
-            print('in a script....')
             for key in vardict:
-                print("Looking for "+key)
                 line = re.sub(r"(" + re.escape(key) + r")", "{{ " + vardict[key] + " }}", line)
 
 
