967541908fa279b3b29708face2c7c4a88a49d8c
Rémy RVD. Van Dyk <rvandyk@axess.local>
2017-06-07 09:23:29 +0200

    js scripts handler

--- twigparse.py
+++ twigparse.py
@@ -6,8 +6,21 @@ def parse(fin,fout):
     o = open(fout, 'w')
 
     currentbox = deque()
+    vardict = dict()
+    script = False
 
     for line in f:
+        if(re.search(r"<script type=\"text/javascript\">",line)):
+            script = True
+        if(re.search(r"</script>",line)):
+            script = False
+        m = re.search(r"<flexy:toJavascript (?P<var_name>.+)={(?P<var_value>.+)}></flexy:toJavascript>",line)
+        if(m):
+            line = ""
+            m.groupdict()
+            print(m['var_name'] + '=' + m['var_value'])
+            vardict.update({m['var_name'] :  m['var_value']})
+
         if(re.search(r"({if)", line)):
             currentbox.append("if")
         if(re.search(r"({foreach)", line)):
@@ -19,13 +32,22 @@ def parse(fin,fout):
             res += ' in ' + ex[0] + " }"
             line = re.sub(r"({foreach:)(.*)[}]+", res, line)
             currentbox.append("for")
-        line = re.sub(r"[:]", " ", line)
-        line = re.sub(r"[{]", "{% ", line)
-        line = re.sub(r"[}]", " %}", line)
-        if(re.search(r"({% end)",line)):
-            line = re.sub(r"({% end)", "{% end" + currentbox.pop(), line)
-        line = re.sub(r"<flexy include src=", "{% include ", line)
-        line = re.sub(r"></flexy include>", " %}", line)
-        line = re.sub(r"\#", "\"", line)
+
+        if(not script):
+            line = re.sub(r"[:]", " ", line)
+            line = re.sub(r"[{]", "{% ", line)
+            line = re.sub(r"[}]", " %}", line)
+            if(re.search(r"({% end)",line)):
+                line = re.sub(r"({% end)", "{% end" + currentbox.pop(), line)
+            line = re.sub(r"<flexy include src=", "{% include ", line)
+            line = re.sub(r"></flexy include>", " %}", line)
+            line = re.sub(r"\#", "\"", line)
+        if(script):
+            print('in a script....')
+            for key in vardict:
+                print("Looking for "+key)
+                line = re.sub(r"(" + re.escape(key) + r")", "{{ " + vardict[key] + " }}", line)
+
+
 
         o.write(line)
