dbec32bd7c2306d1db9390e90a93447d70255cd3
Rémy RVD. Van Dyk <rvandyk@axess.local>
2017-06-15 14:16:02 +0200

    iconv

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ if(len(sys.argv) == 4):
                         f_out = open(sys.argv[3] + '/' + name, 'w')
 
                     f_out.write(parse(f_in.read()))
-                    os.system("iconv -f \"utf8\"  -t \"iso-8859-15\" " + f_out.name + " -c -o " + f_out.name+".utf8" + " --silent && mv -f " + f_out.name+".utf8 " + f_out.name)
+                    if(platform.system() == 'Linux'):
+                        os.system("iconv -f \"utf8\"  -t \"iso-8859-15\" " + f_out.name + " -c -o " + f_out.name+".utf8" + " --silent && mv -f " + f_out.name+".utf8 " + f_out.name)
                     f_in.close()
                     f_out.close()
 
